21b8aee6624d487c5b6a6bb0b7b0e65601147416
RonSala <ron.sala@gmail.com>
2019-11-11 02:54:42 -0500

    Done.

--- app/models/github_repo.rb
+++ app/models/github_repo.rb
@@ -2,9 +2,8 @@ class GithubRepo
 
   attr_accessor :name, :url
 
-    def initialize(hash)
-      # binding.pry
-      @name = hash["name"]
-      @url = hash["html_url"]
-    end
+  def initialize(hash)
+    @name = hash["name"]
+    @url = hash["html_url"]
   end
+end
---
+++ app/services/github_service.rb
@@ -0,0 +1,5 @@
+class GithubService
+  def access_token
+    
+  end
+end
